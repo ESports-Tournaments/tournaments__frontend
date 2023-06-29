17f3b81d360420eb53468209135c665cb8973372
WildRory <kostya.borzistyy@mail.ru>
2023-06-29 21:06:14 +0300

    development/TY-14

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,6 @@ export default function Routing() {
   return (
     <Routes>
       {routes.map((route) => (
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
         <Route key={route.path} path={route.path} Component={route.component} />
       ))}
     </Routes>
