6d8724b1e8764bb50e78ce6338a7b50d5f0abfdd
Zdorovtsev Viktor <v.zdvacew@gmail.com>
2023-07-02 01:01:38 +0700

    add RootLayout

---
+++ src/layout/RootLayout/RootLayout.tsx
@@ -0,0 +1,22 @@
+import type { FC } from 'react';
+import { Outlet } from 'react-router-dom';
+
+import { AppBar } from '~/components/AppBar';
+import { Header } from '~/components/Header';
+
+import style from './RootLayout.module.scss';
+
+export const RootLayout: FC = () => {
+  return (
+    <>
+      <AppBar />
+      <Header />
+
+      <div className={style.content}>
+        <div className={style.container}>
+          <Outlet />
+        </div>
+      </div>
+    </>
+  );
+};
