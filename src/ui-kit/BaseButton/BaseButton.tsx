16fc4f7b6ebc88fa327f4f02568f990eb32eb22f
Julia Popova <yuliapopova.s@yandex.ru>
2023-07-03 21:26:25 +0300

    added basic css variables

--- src/ui-kit/BaseButton/BaseButton.tsx
+++ src/ui-kit/BaseButton/BaseButton.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react';
+import { forwardRef } from 'react';
 
-import styles from './BaseButton.module.scss';
+import style from './BaseButton.module.scss';
 
 type Props = {
   onClick: () => void;
@@ -8,14 +8,12 @@ type Props = {
   name: string;
 };
 
-const BaseButton = React.forwardRef<HTMLButtonElement, Props>(
-  ({ name, ...props }, ref) => {
-    return (
-      <button {...props} className={styles.baseButton} ref={ref}>
-        {name}
-      </button>
-    );
-  },
-);
+const BaseButton = forwardRef<HTMLButtonElement, Props>(({ name, ...props }, ref) => {
+  return (
+    <button {...props} className={style.baseButton} ref={ref}>
+      {name}
+    </button>
+  );
+});
 
 export default BaseButton;
