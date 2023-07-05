69298d4b088ab6e5e71ea90a81344016a93b15ea
Zdorovtsev Viktor <v.zdvacew@gmail.com>
2023-07-05 17:25:10 +0700

    remove comment

--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -9,9 +9,8 @@ type Props = {
 export const MobileMenu: FC<Props> = ({ onClose }) => {
   return (
     <div className={style.root}>
-      {/* Зачем role="presentation"  */}
-      {/* https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/interactive-supports-focus.md#case-this-element-is-not-a-button-link-menuitem-etc-it-is-catching-bubbled-events-from-elements-that-it-contains  */}
       <div className={style.overlay} onClick={onClose} role="presentation" />
+
       <div className={style.container}>
         <div className={style.content}>
           <p>fqefqefqefqefqe</p>
